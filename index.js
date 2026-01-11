24bd6bf246d48d92aa7417ab32b13514cc4277ab
ionnuthub <raducugionut@gmail.com>
2026-01-11 12:52:37 +0100

    uibuild

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
-const cors = require('cors');
+// const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 app.use(express.static('dist'));
